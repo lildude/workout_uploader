15b77e91cd961d0311200975297fd45846f72598
Colin Seymour <colin@github.com>
2014-08-15 18:18:34 +0100

    Starting on fetch uploader

---
+++ lib/fetch_uploader.rb
@@ -0,0 +1,111 @@
+#!/usr/bin/env ruby
+#
+# TODO: Turn this into a plugin
+#
+require "rubygems"
+require "bundler/setup"
+
+require "mechanize"
+require "time"
+require "yaml"
+
+CONFIG = YAML.load_file("config.yml") unless defined? CONFIG
+
+@activity_count = 0
+@upload_count = 0
+
+def log(message, severity = "info")
+  if CONFIG["log_file"]
+    require "logger"
+    logger = Logger.new(CONFIG["log_file"])
+    logger.progname = "workout_uploader"
+    logger.send(:"#{severity}", "#{message}")
+    logger.close
+  end
+end
+
+def startup()
+  unless File.exists?("#{CONFIG['watch_path']}/.fetch.tkr")
+    dot_tkr = File.open("#{CONFIG['watch_path']}/.fetch.tkr", "w+")
+  else
+    dot_tkr = File.open("#{CONFIG['watch_path']}/.fetch.tkr", "r+")
+  end
+
+  last_upload_time = dot_tkr.read
+  if last_upload_time.length > 0
+    last_upload_time = Time.parse(last_upload_time)
+  end
+
+  if last_upload_time.is_a?(Time)
+    puts "Here"
+    files = Dir.glob("#{CONFIG['watch_path']}/*.#{CONFIG['file_type']}", File::FNM_CASEFOLD).
+      select{ |f| File.mtime(f) > last_upload_time }
+  else
+    files = Dir.glob("#{CONFIG['watch_path']}/*.#{CONFIG['file_type']}", File::FNM_CASEFOLD)
+    puts files
+  end
+
+  @activity_count = files.length
+  files.each { |f| upload_file(f) }
+
+  if @activity_count == 0
+    log "No activities to sync"
+  elsif @upload_count == @activity_count
+    last_upload_time = Time.now
+    # Go back to the beginning of the file and update the timestamp
+    dot_tkr.rewind
+    dot_tkr.write(last_upload_time)
+    log "#{@activity_count} activities have been uploaded. Good work!"
+  else
+    log "Some activities were skipped. Look into this.", "warn"
+  end
+  dot_tkr.close
+end
+
+def upload_file(activity)
+
+    log "Syncing Activity - #{File.basename(activity)} to FetchEveryone"
+
+
+    fe = Mechanize.new
+    fe.get('http://www.fetcheveryone.com/') do |page|
+      log "Signing in to FetchEveryone..."
+      mp = page.form_with(:action => "dologin.php") do |f|
+        f.email = CONFIG['plugins']['fetcheveryone']['email']
+        f.password = CONFIG['plugins']['fetcheveryone']['password']
+      end.submit
+
+      puts mp
+    end
+=begin
+    params = { :data_type => "#{CONFIG['file_type']}".downcase, :file => File.open("#{activity}", 'r') }
+
+    begin
+      upload = HTTMultiParty.post(
+        "https://www.strava.com/api/v3/uploads",
+        body: params,
+        headers: { "Authorization" => "Bearer #{CONFIG['plugins']['strava']['token']}" }
+      )
+
+      if upload.code < 400
+        if upload["activity_id"]
+          log "Success! Upload ID: #{upload["activity_id"]}"
+        else
+          log "Success! Though Strava says: #{upload["status"]}"
+        end
+        @upload_count = @upload_count + 1
+      elsif upload.code == 400
+        log upload["error"], 'warn'
+        @upload_count = @upload_count + 1
+      else
+        log "Uh oh, something went wrong uploading the activity: #{upload["message"]}", "error"
+        log "=> Errors: #{upload["errors"]}", "error"
+      end
+
+    rescue Exception => e
+        log "Uh oh, something went wrong uploading the activity: #{e.message}", "fatal"
+    end
+=end
+end
+
+startup()
